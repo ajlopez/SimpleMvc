5556a5b41cbb6e38a0ff9f3c3bc63ef258f4c981
ajlopez <webmaster@ajlopez.com>
2012-05-26 12:28:26 -0300

    template.isTemplateText with tests

---
+++ test/templateText.js
@@ -0,0 +1,13 @@
+
+var $ = require('../lib/template.js')
+  , assert = require('assert');
+  
+assert.ok(!$.isTemplateText());
+assert.ok(!$.isTemplateText(null));
+assert.ok(!$.isTemplateText("Hello, world"));
+assert.ok(!$.isTemplateText("<h1>Hello, world</h1>"));
+
+assert.ok($.isTemplateText("1 + 2 = ${1+2}"));
+assert.ok($.isTemplateText("<#var x = 'My World'; #>Hello, ${x}"));
+assert.ok($.isTemplateText("<#for (var k=1; k<=6; k++) writer.write(k);#>"));
+
